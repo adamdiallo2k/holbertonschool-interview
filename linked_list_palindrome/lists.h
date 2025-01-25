af0bf9c91b149fd48e338b235040d39aa27ca8f1
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-01-25 17:10:41 +0100

    Update lists.h

--- linked_list_palindrome/lists.h
+++ linked_list_palindrome/lists.h
@@ -1,8 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -22,5 +20,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
+int pop(listint_t **stack);
+void push(listint_t **stack, int n);
 
 #endif /* LISTS_H */
