f71d321a21640c59c287c37a4affa4489d4da48a
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-03-27 16:05:44 +0100

    Update 0-is_palindrome.c

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,88 +1,37 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * is_palindrome - test if a linked list is a palindrome
- * @head: pointer to pointer of first node in linked list
- * Return: 0 if not, 1 if it is a palindrome
+ * is_palindrome_recursive - checks if two halves of a linked list are equal
+ * @left: pointer to the left half of the linked list
+ * @right: pointer to the right half of the linked list
+ * Return: 1 if equal, 0 otherwise
+ */
+int is_palindrome_recursive(listint_t **left, listint_t *right)
+{
+    if (right == NULL)
+        return 1;
+
+    if (!is_palindrome_recursive(left, right->next))
+        return 0;
+
+    if ((*left)->n != right->n)
+        return 0;
+
+    *left = (*left)->next;
+    return 1;
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to the head of the linked list
+ * Return: 1 if it is a palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head;
-	listint_t *fast = *head;
-	listint_t *stack = NULL;
+    if (*head == NULL || (*head)->next == NULL)
+        return 1;
 
-	if (*head == NULL || (*head)->next == NULL)
-	{
-		return (1);
-	}
-
-	while (fast != NULL && fast->next != NULL)
-	{
-		push(&stack, slow->n);
-		slow = slow->next;
-		fast = fast->next->next;
-	}
-
-	/* if linked list is odd */
-	if (fast != NULL)
-	{
-
-		slow = slow->next;
-	}
-
-	while (slow != NULL)
-	{
-		if (pop(&stack) != slow->n)
-		{
-			free(stack);
-			return (0);
-		}
-		slow = slow->next;
-	}
-	free(stack);
-	return (1);
-}
-
-/**
- * push - pushes an element on top of stack
- * @stack: pointer to pointer to the top of the stack
- * @n: data to be pushed onto the stack
- */
-void push(listint_t **stack, int n)
-{
-	listint_t *new_node = malloc(sizeof(listint_t));
-
-	if (new_node == NULL)
-	{
-		exit(EXIT_FAILURE);
-	}
-
-	new_node->n = n;
-	new_node->next = *stack;
-	*stack = new_node;
-}
-/**
- * pop - pops an element from the top of the stack
- * @stack: pointer to pointer to the top of the stack
- * Return: data of the popped element
- */
-int pop(listint_t **stack)
-{
-listint_t *temp;
-int n;
-
-if (*stack == NULL)
-{
-exit(EXIT_FAILURE);
-}
-
-temp = *stack;
-n = temp->n;
-
-*stack = (*stack)->next;
-free(temp);
-
-return (n);
+    return is_palindrome_recursive(head, *head);
 }
