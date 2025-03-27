dccaaad4042bcb4bad354156a290819d41fc317f
adamdiallo2k <47924788+adamdiallo2k@users.noreply.github.com>
2025-03-27 16:06:40 +0100

    Update lists.h

--- linked_list_palindrome/lists.h
+++ linked_list_palindrome/lists.h
@@ -20,7 +20,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-int pop(listint_t **stack);
-void push(listint_t **stack, int n);
 
 #endif /* LISTS_H */
